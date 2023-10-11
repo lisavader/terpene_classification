454754b79b02b07194b74fc1bf7ba60003706fe8
Lisa Vader <lisavader@users.noreply.github.com>
2023-10-11 16:49:03 +0200

    Add manual curation of enzyme subclasses

--- extract_metadata.py
+++ extract_metadata.py
@@ -3,6 +3,15 @@ import argparse
 from InterproRecord import InterproRecord
 from fasta_parsing import read_fasta
 
+def curate_record(record):
+    if record.protein_name == "Aristolochene_synthase_in_complex_with_12,13_Difluorofarnesyl_diphosphate":
+        record.enzyme_subclass = "sesquiterpene synthase"
+    if "gibberellin_cluster-ggpp-synthase" in record.protein_name.lower() or record.protein_name in \
+        ["Geranylgeranyl_pyrophosphate_synthetase,_putative_[includes:_dimethylallyltranstransferase_(Ec_2.5.1.1)_geranyltranstransferase_(Ec_2.5.1.10)_farnesyltranstransferas_(Ec_2.5.1.29)]",
+        "Geranylgeranyl_pyrophosphate_synthase_(Fusicoccadiene_synthase)"]:
+        record.enzyme_subclass = "GGPP synthase"
+    return record
+
 def write_json(records, json_out):
     for record in records:
         file_path = json_out+"/"+record.accession+".json"
@@ -17,6 +26,7 @@ def main(input, json_dir):
         record = InterproRecord(header,seq)
         record.assign_enzyme_labels()
         record.assign_taxonomic_labels()
+        record = curate_record(record)
         records.append(record)
     write_json(records, json_dir)
 
