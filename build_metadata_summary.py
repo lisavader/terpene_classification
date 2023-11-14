70cc4a9118e4c0d94836132d97fcb0988f8f35d7
Lisa Vader <lisavader@users.noreply.github.com>
2023-11-14 09:56:31 +0100

    Update columns according to new records classes

--- build_metadata_summary.py
+++ build_metadata_summary.py
@@ -7,7 +7,7 @@ def main(json_dir, output):
     json_files = glob.glob(json_dir+"/*.json")
     with open(output, 'w') as csv_out:
         writer = csv.writer(csv_out)
-        header = ["accession","protein_name","enzyme_class","enzyme_subclass","organism_name","organism_division","review_status"]
+        header = ["accession","database","protein_name","enzyme_type","organism_name","organism_division","reviewed"]
         writer.writerow(header)
         for json_file in json_files:
             row = []
