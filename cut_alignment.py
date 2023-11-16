719895bf520c72a2cc9c2e32cfcef7e06f4cdfbb
Lisa Vader <lisavader@users.noreply.github.com>
2023-11-16 12:38:42 +0100

    Allow cutting out multiple regions at once

--- cut_alignment.py
+++ cut_alignment.py
@@ -1,19 +1,32 @@
 import argparse
+import itertools
 from Bio import AlignIO, SeqIO
+from Bio.Align import MultipleSeqAlignment, SeqRecord
 
-def main(alignment_in, alignment_out, start, stop, format):
-    if start:
-        start = start-1     #Python indexes start at 0 instead of 1
+def main(alignment_in, alignment_out, regions, format):
     alignment = AlignIO.read(alignment_in, format)
-    cut_alignment = alignment[:, start:stop]
-    SeqIO.write(cut_alignment, alignment_out, format)
+    fields = regions.split(',')
+    alignment_regions = []
+    for field in fields:
+        if ':' in field:
+            start,stop = map(int, field.split(':'))
+            start = start-1 #Input index starts at 1 instead of 0
+            alignment_region = alignment[:, start:stop]
+        else:
+            alignment_region = alignment[:, int(field)]
+        alignment_regions.append(alignment_region)
+    #paste the regions together
+    merged_alignment = alignment_regions[0]
+    for align in alignment_regions[1:]:
+        for i, record in enumerate(align):
+            merged_alignment[i].seq+=record.seq
+    AlignIO.write(merged_alignment, alignment_out, format)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("alignment_in", type=str, help="Input alignment")
     parser.add_argument("alignment_out", type=str, help="Output alignment")
-    parser.add_argument("--start", type=int, help="First column to include in cut. Default = 1 (first index of the alignment)", default=None)
-    parser.add_argument("--stop", type=int, help="Last column to include in cut. Default = End of the alignment", default=None)
+    parser.add_argument("regions", type=str, help="Which region(s) to cut. If multiple, separate by a comma. E.g. 1:8,10:12,21.")
     parser.add_argument("--format", type=str, default="fasta", help="Format of the alignment (Default: fasta)")
     args = parser.parse_args()
-    main(args.alignment_in, args.alignment_out, args.start, args.stop, args.format)
+    main(args.alignment_in, args.alignment_out, args.regions, args.format)
