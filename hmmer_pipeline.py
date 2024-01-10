970ef3db1a201726b116ab6e19591e0ef1a3ef86
Lisa Vader <lisavader@users.noreply.github.com>
2024-01-10 10:20:18 +0100

    Make supplying antismash cutoffs optional

--- hmmer_pipeline.py
+++ hmmer_pipeline.py
@@ -20,18 +20,20 @@ def queryresult_to_df(queryresult):
 def main(hmmfile, fasta, hmmdetails, output):
     queryresult = run_hmmscan(hmmfile, fasta)
     hmmer_df = queryresult_to_df(queryresult)
-    details = pd.read_csv(hmmdetails, delimiter='\t', header=None, index_col=0)
-    cutoffs = details.iloc[:,1]
-    cutoff_dict = cutoffs.to_dict()
-    hmmer_df["cutoff_score"] = hmmer_df["id"].apply(lambda x: cutoff_dict.get(x))
-    hmmer_df["significant"] = np.where(hmmer_df["bitscore"] >= hmmer_df["cutoff_score"], "True", "False")
+    if hmmdetails:
+        details = pd.read_csv(hmmdetails, delimiter='\t', header=None, index_col=0)
+        cutoffs = details.iloc[:,1]
+        cutoff_dict = cutoffs.to_dict()
+        hmmer_df["cutoff_score"] = hmmer_df["id"].apply(lambda x: cutoff_dict.get(x))
+        hmmer_df["significant"] = np.where(hmmer_df["bitscore"] >= hmmer_df["cutoff_score"], "True", "False")
     hmmer_df.to_csv(output, index=False, sep='\t')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("hmmfile", type=str, help="the path to a HMM file to use in scanning")
     parser.add_argument("fasta", type=str, help="a string containing input sequences in fasta format")
-    parser.add_argument("hmmdetails", type=str, help="Path to the antiSMASH file hmmdetails.txt describing cutoffs for each profile")
+    parser.add_argument("--hmmdetails", type=str, help="Optional: Path to the antiSMASH file hmmdetails.txt describing cutoffs for each profile."+
+                        "This is used to calculate if hits are 'significant' (above the cut-off)")
     parser.add_argument("output", type=str, help="Path to output .tsv file")
     args = parser.parse_args()
     main(args.hmmfile, args.fasta, args.hmmdetails, args.output)
