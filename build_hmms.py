eff6c0f75a0e115cc21602b196ed04d43e7c9f62
Lisa Vader <lisavader@users.noreply.github.com>
2024-11-08 17:48:27 +0100

    Add force option to overwrite output files

--- build_hmms.py
+++ build_hmms.py
@@ -7,7 +7,7 @@ from subprocess import Popen, PIPE
 
 from fasta_parsing import read_fasta
 
-def main(accessions_dir, hmm_dir, fasta, leave_one_out):
+def main(accessions_path, hmm_dir, fasta, leave_one_out, force):
     fasta_dict = read_fasta(fasta)
     for accessions_file in glob.glob(os.path.join(accessions_dir,"*")):
         hmm_name = os.path.basename(accessions_file).strip(".txt")
@@ -22,7 +22,7 @@ def main(accessions_dir, hmm_dir, fasta, leave_one_out):
             for header,seq in fasta_dict.items():
                 if header == accession:
                     sequences[header] = seq
-        build_hmm(sequences, hmm_dir, hmm_name)
+        build_hmm(sequences, hmm_dir, hmm_name, force)
 
         #Build leave-one-out hmms
         if leave_one_out:
@@ -33,11 +33,11 @@ def main(accessions_dir, hmm_dir, fasta, leave_one_out):
                     if accession.endswith(base_accession):
                         del sequences_loo[accession]
                 hmm_name_loo = hmm_name+"_"+base_accession
-                build_hmm(sequences_loo, hmm_dir, hmm_name_loo)
+                build_hmm(sequences_loo, hmm_dir, hmm_name_loo, force)
 
-def build_hmm(sequences, hmm_dir, hmm_name):
+def build_hmm(sequences, hmm_dir, hmm_name, force):
     hmm_file = os.path.join(hmm_dir, hmm_name+".hmm")
-    if os.path.isfile(hmm_file):
+    if os.path.isfile(hmm_file) and not force:
         logging.info(f"Skipping {hmm_name}, file already exists at {hmm_file}")
         return
     else:
@@ -67,7 +67,8 @@ if __name__ == "__main__":
     parser.add_argument("fasta", type=str, help="Fasta file that contains the sequences for each accession")
     parser.add_argument("--leave_one_out", action="store_true", help="Build additional hmms with one of the sequences left out.")
     parser.add_argument("-v", "--verbose", action="store_const", dest="loglevel", const=logging.INFO, help="Enable verbose mode")
+    parser.add_argument("-f", "--force", action="store_true", help="Overwrite existing files")
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
     #Run the main script
-    main(args.accessions_dir, args.hmm_dir, args.fasta, args.leave_one_out)
+    main(args.accessions_path, args.hmm_dir, args.fasta, args.leave_one_out, args.force)
